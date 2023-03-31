b180c306f41060fda15b816a9016e40d18c3de63
John Benedict Geli <john.benedict.geli.ipi@prulifeuk.com.ph>
2023-03-31 16:20:35 +0800

    initial commit

---
+++ backend/routes/goalRoutes.js
@@ -0,0 +1,15 @@
+//Connect Controller Functions.
+
+
+const express = require('express')
+const router = express.Router()
+const { getGoals, setGoal, updateGoal, deleteGoal } = require('../controllers/goalController')
+
+// To set chain routesin  goalController.js
+router.route('/').get(getGoals).post(setGoal)
+
+router.route('/:id').delete(deleteGoal).put(updateGoal)
+
+
+
+module.exports = router
